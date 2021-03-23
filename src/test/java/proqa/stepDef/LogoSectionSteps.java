8c129ff27c80c14cdb8afc1c1cd6cddad0e3819a
ZennT <zenn.diamond@gmail.com>
2021-03-22 23:53:32 -0700

    Added LogoSection.feature, LogoSection.java and LogoSectionSteps.java

---
+++ src/test/java/proqa/stepDef/LogoSectionSteps.java
@@ -0,0 +1,14 @@
+package proqa.stepDef;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class LogoSectionSteps {
+    @Given("User is on Landing page")
+    public void userIsOnLandingPage() {
+    }
+
+    @When("User verifies that logos are displayed on the home page")
+    public void userVerifiesThatLogosAreDisplayedOnTheHomePage() {
+    }
+}
