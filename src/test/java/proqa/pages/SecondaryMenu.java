97d3b323565baa6054aac8166a682e8c177a6cea
Esizsu <esra_izgi@hotmail.com>
2021-03-24 17:56:34 -0700

    added some webelements

--- src/test/java/proqa/pages/SecondaryMenu.java
+++ src/test/java/proqa/pages/SecondaryMenu.java
@@ -1,5 +1,7 @@
 package proqa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import proqa.utils.MyDriver;
 
@@ -8,6 +10,25 @@ public class SecondaryMenu {
     public SecondaryMenu() {
         PageFactory.initElements(MyDriver.get(), this);
     }
+
+    @FindBy (xpath = "(//a[@class=\"custom_url\"])[1]")
+    public WebElement email;
+
+    @FindBy (xpath = "//span[normalize-space()='info@proqa.dev']")
+    public WebElement emailtext;
+
+    @FindBy (xpath = "(//a[@class = \"custom_url\"])[1]")
+    public WebElement address;
+
+    @FindBy (xpath = "(//i[@class=\"flaticon-twitter\"])[1]")
+    public WebElement twitter;
+
+    @FindBy (xpath = "(//span[normalize-space()='ProQA'])[1]")
+    public WebElement twittertext;
+
+    @FindBy (xpath = "(//i[@class=\"flaticon-linked-in\"])[1]")
+    public WebElement linkedin;
+
 
 
 }
