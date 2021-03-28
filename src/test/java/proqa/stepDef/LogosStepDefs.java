723b28548e4a78a58e69b0e013219431bfa01e34
ZennT <zenn.diamond@gmail.com>
2021-03-27 18:17:26 -0700

    verified homepage url on LogosStepDefs.java

--- src/test/java/proqa/stepDef/LogosStepDefs.java
+++ src/test/java/proqa/stepDef/LogosStepDefs.java
@@ -2,7 +2,10 @@ package proqa.stepDef;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import proqa.pages.LogosPage;
+import proqa.utils.ConfigurationReader;
+import proqa.utils.MyDriver;
 
 public class LogosStepDefs extends Base {
 
@@ -10,6 +13,12 @@ public class LogosStepDefs extends Base {
 
     @Given("User is on Landing page")
     public void userIsOnLandingPage() {
+
+        String expected = "https://www.proqa.dev/";
+        String actual = MyDriver.get().getCurrentUrl();
+        Assert.assertEquals(expected, actual);
+        logger.info("proqa.dev website url is {}", actual);
+        waitSomeTime(3000L);
     }
 
     @When("Verifies that Logos are present")
