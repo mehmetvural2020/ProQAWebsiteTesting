1f43d62bf3b322a293851310e42b1dede9ebbf26
Mehmet Vural <mehmetvurall@gmail.com>
2021-04-02 02:21:08 +0700

    Updated some files.

--- src/test/java/proqa/pages/SlidePage.java
+++ src/test/java/proqa/pages/SlidePage.java
@@ -31,9 +31,12 @@ public class SlidePage {
     @FindBy(xpath = "//h3[contains(text(), 'Free Consultation')]")
     public WebElement freeConsultation;
 
-
     @FindBy(xpath = "//rs-slide[@data-key='rs-3']")
     public WebElement slideImage3;
 
+    @FindBy(xpath = "//rs-layer[@id='slider-1-slide-3-layer-7']")
+    public WebElement slideTitle3;
 
+    @FindBy(xpath = "//rs-arrow[@class='tp-leftarrow tparrows persephone']")
+    public WebElement leftArrow;
 }
--- src/test/java/proqa/stepDef/SlideStepDefs.java
+++ src/test/java/proqa/stepDef/SlideStepDefs.java
@@ -57,7 +57,7 @@ public class SlideStepDefs extends Base {
     public void userVerifiesThatContactSlideTitleIsPresent() {
         String expected = "free consultation";
         moveToElement(slidePage.slideTitle2);
-        waitSomeTime(3000L);
+        waitSomeTime(1000L);
         textIsDisplayedAndEnabled(expected, slidePage.slideTitle2);
     }
 
@@ -66,48 +66,86 @@ public class SlideStepDefs extends Base {
         click(slidePage.contactNowBtn);
         waitSomeTime(1000L);
         String expected = "Free Consultation";
-
+        textIsDisplayedAndEnabled(expected, slidePage.freeConsultation);
     }
 
     @Then("user clicks on right arrow and verify the last slide")
     public void userClicksOnRightArrowAndVerifyTheLastSlide() {
-
-
+        moveToElement(slidePage.rightArrow);
+        waitSomeTime(1000L);
+        click(slidePage.rightArrow);
+        waitSomeTime(2000L);
+        imageIsDisplayedAndEnabled(slidePage.slideImage3);
     }
 
     @Given("user is in landing page and click next button two times")
     public void userIsInLandingPageAndClickNextButtonTwoTimes() {
+        moveToElement(slidePage.rightArrow);
+        waitSomeTime(1000L);
+        click(slidePage.rightArrow);
+        waitSomeTime(1000L);
+        click(slidePage.rightArrow);
+        waitSomeTime(1000L);
     }
 
     @When("user verifies that smiling girl image is present")
     public void userVerifiesThatSmilingGirlImageIsPresent() {
+        imageIsDisplayedAndEnabled(slidePage.slideImage3);
     }
 
     @And("user verifies that the last slide title is present")
     public void userVerifiesThatTheLastSlideTitleIsPresent() {
+        String expected = "fast delivery";
+        textIsDisplayedAndEnabled(expected, slidePage.slideTitle3);
     }
 
     @Then("user clicks on right arrow and verify main slide")
     public void userClicksOnRightArrowAndVerifyMainSlide() {
+        moveToElement(slidePage.rightArrow);
+        waitSomeTime(1000L);
+        click(slidePage.rightArrow);
+        waitSomeTime(1000L);
+        imageIsDisplayedAndEnabled(slidePage.slideImage1);
     }
 
     @Given("user is in landing page")
     public void userIsInLandingPage() {
+        String expected = MyDriver.get().getCurrentUrl();
+        String actual = "https://www.proqa.dev/";
+        verifyTheText(expected, actual);
     }
 
     @When("user clicks on the left arrow on slider")
     public void userClicksOnTheLeftArrowOnSlider() {
+        moveToElement(slidePage.leftArrow);
+        waitSomeTime(1000L);
+        click(slidePage.leftArrow);
+        waitSomeTime(1000L);
     }
 
     @And("user verifies that third slide title is present")
     public void userVerifiesThatThirdSlideTitleIsPresent() {
+        String expected = "fast delivery";
+        textIsDisplayedAndEnabled(expected, slidePage.slideTitle3);
     }
 
     @And("user clicks on the left arrow and verify the second slide title is present")
     public void userClicksOnTheLeftArrowAndVerifyTheSecondSlideTitleIsPresent() {
+        moveToElement(slidePage.leftArrow);
+        waitSomeTime(1000L);
+        click(slidePage.leftArrow);
+        waitSomeTime(1000L);
+        String expected = "free consultation";
+        textIsDisplayedAndEnabled(expected, slidePage.slideTitle2);
     }
 
     @Then("user clicks on the left arrow and verify the first slide title is present")
     public void userClicksOnTheLeftArrowAndVerifyTheFirstSlideTitleIsPresent() {
+        moveToElement(slidePage.leftArrow);
+        waitSomeTime(1000L);
+        click(slidePage.leftArrow);
+        waitSomeTime(1000L);
+        String expected = "agile testing";
+        textIsDisplayedAndEnabled(expected, slidePage.slideTitle1);
     }
 }
