9e9c2a457708f96e5ef53f8fee9ebe23f2a07e38
Mehmet Vural <mehmetvurall@gmail.com>
2021-04-28 02:00:39 +0700

    Created more classes.

--- src/test/java/proqa/Pages/BlogHomePage.java
+++ src/test/java/proqa/Pages/BlogHomePage.java
@@ -1,5 +1,7 @@
 package proqa.Pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import proqa.utils.MyDriver;
 
@@ -7,6 +9,43 @@ public class BlogHomePage {
     public BlogHomePage() {
         PageFactory.initElements(MyDriver.get(), this);
     }
+
+    @FindBy(linkText = "Blog")
+    public WebElement blog;
+
+    @FindBy(xpath = "(//div[@class=\"blog-readmore-wrap\"])[1]")
+    public WebElement arrow1;
+
+    @FindBy(xpath = "(//div[@class=\"post-nav-link\"])[1]")
+    public WebElement previous1;
+
+    @FindBy(xpath = "(//nav[@class=\"bread-crumbs\"])[1]")
+    public WebElement homeBlogText;
+
+    @FindBy(xpath = "//nav[@class=\"bread-crumbs\"]/a[1]")
+    public WebElement homeBlog;
+
+    @FindBy(xpath = "//*[contains(text(),'The Need for Test Automation')]")
+    public WebElement firstPost;
+
+    @FindBy(xpath = "//*[contains(text(),'What is Software Testing?')]")
+    public WebElement secondPost;
+
+    @FindBy(xpath = "//*[contains(text(),'What is SDLC?')]")
+    public WebElement thirdPost;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
--- src/test/java/proqa/stepDef/BlogHomePageSteps.java
+++ src/test/java/proqa/stepDef/BlogHomePageSteps.java
@@ -2,21 +2,45 @@ package proqa.stepDef;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import proqa.Pages.BlogHomePage;
+import proqa.utils.MyDriver;
 
 public class BlogHomePageSteps extends Base{
     BlogHomePage blogHomePage = new BlogHomePage();
 
     @Given("user is in landing page")
     public void userIsInLandingPage() {
+        click(blogHomePage.blog);
+        waitSomeTime(2000L);
+//        click(blogHomePage.arrow1);
+//        waitSomeTime(3000L);
+//        moveToElement(blogHomePage.previous1);
+//        waitSomeTime(2000L);
+//        click(blogHomePage.previous1);
+//        moveAndClickToElement(blogHomePage.previous1);
+//        waitSomeTime(3000L);
     }
 
     @When("user verifies Home link")
     public void userVerifiesHomeLink() {
+        verifyTheText("HomeBlog", blogHomePage.homeBlogText.getText());
     }
 
     @And("user clicks on Home")
     public void userClicksOnHome() {
+        waitSomeTime(2000L);
+        click(blogHomePage.homeBlog);
+    }
+
+    @Then("user verifies Homepage url")
+    public void userVerifiesHomepageUrl() {
+        waitSomeTime(2000L);
+        String expected = "https://www.proqa.dev/";
+        String actual =  MyDriver.get().getCurrentUrl();
+
+        verifyTheText(expected, actual);
+        waitSomeTime(1000L);
     }
 }
