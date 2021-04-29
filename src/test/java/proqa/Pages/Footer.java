2740b5a3db5458f9938241d27d7682650e10f469
Esizsu <esra_izgi@hotmail.com>
2021-04-28 18:57:21 -0700

    updated Footer.java

--- src/test/java/proqa/Pages/Footer.java
+++ src/test/java/proqa/Pages/Footer.java
@@ -17,6 +17,26 @@ public class Footer {
     @FindBy(xpath = "//h3[contains (text(),'Subscribe')]")
     public WebElement Subscribe;
 
+    @FindBy(xpath = "//input[@id='wpforms-1842-field_2']")
+    public WebElement EnterEmail;
+
+    @FindBy(xpath = "//button[@id='wpforms-submit-1842']")
+    public WebElement Arrow;
+
+    @FindBy(xpath = "//p[contains(text(),'Thanks for contacting us! We will be in touch with')]")
+    public WebElement Message;
+
+
+
+    @FindBy(xpath = "(//i[@class='flaticon-twitter'])[2]")
+    public WebElement Twitter;
+
+    @FindBy(xpath = "(//i[@class='flaticon-linked-in'])[2]")
+    public WebElement Linkedin;
+
+    @FindBy(xpath = "(//span[contains (text(),'Chicago, IL')])[2]")
+    public WebElement Address;
+
 
 }
 
